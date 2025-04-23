ef86f93959966e99bb994534d9c3d601427d0d28
Kongkon Bora <kbora3525@gmail.com>
2025-04-23 17:59:14 +0530

    Remove the smallest

--- remove Smallest.c
+++ remove Smallest.c
@@ -1,18 +1,48 @@
 #include<stdio.h>
-#include<stdbool.h>
-bool RemoveElements(int *arr,int n){
-    for(){
+#include<stdlib.h>
 
+int RemoveSmallest(){
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,res,j;
+    res=n;
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
     }
-}
-int main(){
+    for(i=0;i<n;i++){
+        if(arr[i]==-1)continue;
+        for(j=i+1;j<n;j++){
+        if(abs(arr[i] - arr[j]) <= 1 ){
+            if(arr[i]<arr[j]){
+                arr[i]=-1;
+                res--;
+            }else{
+                arr[j]=-1;
+                res--;
+            }
+        }
+     }
+    }
+    return res;
+};
+int main()
+{
     int cases;
     scanf("%d",&cases);
-    bool result[cases];
-    while(cases>0){
-        int n;
-        scanf("%d",n);
-        int temp[n];
+    int arr[cases],i,res;
+    for(i=0;i<cases;i++){
+        res=RemoveSmallest();
+        arr[i] = res;
     }
-    return  ;
+    for(i=0;i<cases;i++){
+        switch (arr[i]) {
+            case 1:
+                printf("YES\n");
+            break;
+            default:
+                printf("NO\n");
+            break;
+        }
+    }
+    return 0;
 }
